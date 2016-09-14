962325885c9419cf0b210de9dfea9b0853d1a1a2
Emily <kisa411@gmail.com>
2016-09-14 14:25:55 -0500

    jQuery

--- CodingExercises/reactiveContent.js
+++ CodingExercises/reactiveContent.js
@@ -1,3 +1,30 @@
 $( document ).ready(function() {
 	console.log("ready!");
+	$("#heart").hover(function(){
+        
+    });
+	$("#horses").hover(function(){
+        
+    });
+	$("#rib").hover(function(){
+        
+    });
+	$("#vessel").hover(function(){
+        
+    });
+	$("#tongue").hover(function(){
+        
+    });
+	$("#duck").hover(function(){
+        
+    });
+	$("#water").hover(function(){
+        
+    });
+	$("#digest").hover(function(){
+        
+    });
+	$("#squirrel").hover(function(){
+        
+    });
 });
