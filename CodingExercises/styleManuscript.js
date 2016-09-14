f35b154563967d76ce305fe10912ade01b4b4bb1
Emily <kisa411@gmail.com>
2016-09-14 08:53:49 -0500

    styleManuscript

--- CodingExercises/styleManuscript.js
+++ CodingExercises/styleManuscript.js
@@ -1,77 +1,75 @@
-//button 1 - increase font size
-window.onload=function(){
-	var el = document.getElementById("incFont");
-	el.addEventListener("click", incSize); 
-}
 
-function incSize(fontChange=2) {
-	console.log("incsize function called");
-    var div = document.getElementById("information");
-	var originalSize = div.style.fontSize.replace("px", "");
+ 
+$(document).ready(function(){
+	//button 1 - increase font size
+	$("#incFont").click(function() {
+		console.log("incsize function called");
+		var fontSize = parseInt($("body").css("font-size"));
+		console.log(fontSize);
+		fontSize = fontSize + 2 + "px";
+		console.log(fontSize);
+		$("body").css({'font-size':fontSize});
+	});
+	//button 2 - drecrease font size
+	$("#decFont").click(function() {
+		console.log("decsize function called");
+		var fontSize = parseInt($("body").css("font-size"));
+		console.log(fontSize);
+		if (fontSize>10) { //only decrease font size until 4px
+			fontSize = fontSize - 2 + "px";
+			console.log(fontSize);
+			$("body").css({'font-size':fontSize});
+		}
+	});
+	//button 3 - change background color
+	$("#backColor").click(function() {
+		console.log("background color function called");
+		if (document.body.style.backgroundColor != "blue") {
+			document.body.style.backgroundColor = "blue";
+			document.body.style.color = "white";
+		} else {
+			document.body.style.backgroundColor = "white";
+			document.body.style.color = "black";
+		}
+	});
+	//button 4 - change text color
+	$("#textColor").click(function() {
+		console.log("text color function called");
+		var div = document.getElementById("information");
+		if (div.style.color != "purple") {
+			div.style.color = "purple";
+		} else {
+			div.style.color = "black";
+		}
+	});
+	//button 5 - increase title size
+	$("#titleSize").click(function() {
+		console.log("title size function called");
+		var fontSize = parseInt($("h4").css("font-size"));
+		console.log(fontSize);
+		fontSize = fontSize + 2 + "px";
+		console.log(fontSize);
+		$("h4").css({'font-size':fontSize});
+	});
+	//button 6 - hide buttons
+	$("#hidden").click(function() {
+		console.log("hide function called");
+		$(".hide").hide();
+	});
+	//button 7 - show buttons
+	$("#show").click(function() {
+		console.log("show function called");
+		$(".hide").show();
+	});
+});
 
-	div.style.fontSize = parseInt(originalSize) + parseInt(fontChange) + "px";
-}
 
-//button 2 - drecrease font size
-var el = document.getElementById("decFont");
-el.addEventListener("click", decSize); 
 
-function decSize(fontChange=2) {
-	console.log("decSize function called!");
-    var div = document.getElementById("information");
-	var originalSize = div.style.fontSize.replace("px", "");
 
-	div.style.fontSize = parseInt(originalSize) + parseInt(fontChange) + "px";
-}
 
-//button 3 - change background color
-window.onload=function(){
-	var el = document.getElementById("backColor");
-	el.addEventListener("click", changeBackgroundColor); 
-}
 
-function changeBackgroundColor() {
-		console.log("backgroundcolor function called!");
-    if (document.body.style.backgroundColor != "blue") {
-		document.body.style.backgroundColor = "blue";
-		document.body.style.fontColor = "white";
-	} else {
-		document.body.style.backgroundColor = "white";
-		document.body.style.fontColor = "black";
-	}
-}
 
-//button 4 - change text color
-window.onload=function(){
-	var el = document.getElementById("textColor");
-	if (el) {
-		el.addEventListener("click", changeTextColor); 
-}
 
-function changeTextColor() {
-		console.log("textcolor function called!");
-    var div = document.getElementById("information");
-	if (div.style.color != "purple") {
-		div.style.color = "purple";
-	} else {
-		div.style.color = "black";
-	}
-}
 
-//button 5 - change picture size
-window.onload=function(){
-	var el = document.getElementById("titleSize");
-	el.addEventListener("click", increaseTitle); 
-}
-var el = document.getElementById("titleSize");
-el.addEventListener("click", increaseTitle); 
-
-function increaseTitle(fontChange=10) {
-		console.log("title function called!");
-    var div = document.getElementById("title");
-	var originalSize = div.style.fontSize.replace("px", "");
-
-	div.style.fontSize = parseInt(originalSize) + parseInt(fontChange) + "px";
-}
 
 
